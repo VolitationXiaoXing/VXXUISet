97b84150e3f0d7d5de56a496f7727be22ec944e2
VolitationXiaoXing <752061676@qq.com>
2017-05-27 15:27:04 +0800

    搭建OC项目

---
+++ VXXUISet/VXXUISet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VXXUISet
+//
+//  Created by Volitation小星 on 17/5/27.
+//  Copyright © 2017年 VolitationXiaoXing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VXXUISet/VXXUISet/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  VXXUISet
+//
+//  Created by Volitation小星 on 17/5/27.
+//  Copyright © 2017年 VolitationXiaoXing. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
